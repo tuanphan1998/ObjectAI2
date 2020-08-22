5118199268d0ac239b815328b9a306195a3a1387
TUanphan nam <phantuanw.o.t@gmail.com>
2020-08-22 21:35:09 +0700

    Updates

---
+++ precache-manifest.71453de372f762e8cb6158d716496f62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da270698d4cff59130cc",
+    "url": "/static/css/main.4e35fe55.chunk.css"
+  },
+  {
+    "revision": "da270698d4cff59130cc",
+    "url": "/static/js/main.d041cdf6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d1aacd4adb39b176aedf",
+    "url": "/static/js/2.bb23ef20.chunk.js"
+  },
+  {
+    "revision": "32d42845c0d8cfc98db2b7726a8442a6",
+    "url": "/index.html"
+  }
+];
